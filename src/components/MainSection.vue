4a50e057c091ba5744f1e78176c1c11e9fe3801e
Lucas Werner <lwerner27@gmail.com>
2019-02-06 21:41:43 -0600

    changed main image to jpg

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -26,7 +26,7 @@
     }
     .slide {
         height: 100vh;
-        background: linear-gradient( rgba(0, 19, 64, 0.6), rgba(0, 19, 64, 0.6) ), url('../assets/MinneapolisSkylineB-min.png');
+        background: linear-gradient( rgba(0, 19, 64, 0.6), rgba(0, 19, 64, 0.6) ), url('../assets/MinneapolisSkylineB-min.jpg');
         background-size: cover;
     }
     .main-title {
